8c9a5cf16b7d3edebc443d9322b170ceebc04fe2
Pedro Cattori <pcattori@gmail.com>
2016-11-02 01:39:54 -0400

    moved eco to fetch/

--- fetch/eco.py
+++ fetch/eco.py
@@ -3,13 +3,16 @@ import collections
 import re
 import requests
 
+from require import require
+Board = require('./board.py').Board
+
 # utils
 
 EcoEntry = collections.namedtuple('EcoEntry', ['eco', 'name', 'moves'])
 
 def parse_moves(moves):
     moves = re.split(r'\d+\.', moves)[1:]
-    return tuple(tuple(move.strip().split()) for move in moves)
+    return tuple(ply for move in moves for ply in move.strip().split())
 
 # scrape
 
@@ -21,12 +24,20 @@ lines = ecotree.find_all('div', class_='line')
 openings = set()
 for line in lines:
   eco, name = line.find('div', class_='opname').a.string.split(' ', 1)
+  name = name.strip()
   moves = line.find('div', class_='opmoves')
   if moves is None:
     moves = line.find('div', class_='fright')
   moves = parse_moves(moves.string)
   openings.add(EcoEntry(eco, name, moves))
 
-print(len(openings))
-print(next(iter(openings)))
+# convert to stateless FEN
+position_names = {}
+for eco, name, moves in openings:
+    board = Board()
+    for move in moves:
+        board.push_san(move)
+    position_names[board.stateless_fen()] = (eco, name)
 
+print(len(position_names))
+print(next(iter(position_names.items())))
