3816a47511743f2d38037e50044e077c4404f841
Pedro Cattori <pcattori@gmail.com>
2016-12-21 00:32:18 -0500

    added eco fetcher eco mapper in utils

--- evolveyourchess/utils.py
+++ evolveyourchess/utils.py
@@ -1,3 +1,4 @@
+import evolveyourchess as eyc
 import chess.pgn as pgn
 
 def parse_pgns(f):
@@ -6,4 +7,13 @@ def parse_pgns(f):
         if game is None:
             break
         yield game
+
+def eco_from_position(openings):
+    position_lookup = {}
+    for eco, name, moves in openings:
+        board = eyc.Board()
+        for move in moves:
+            board.push_san(move)
+        position_lookup[board.FEN()] = (eco, name)
+    return position_lookup
 
--- fetch/eco.py
+++ fetch/eco.py
@@ -1,33 +1,16 @@
 from bs4 import BeautifulSoup
 import collections
+import evolveyourchess as eyc
 import json
+import os
 import re
 import requests
-import os
-
-from require import require
-Board = require('../lib/board.py').Board
-config = require('../config.py')
-
-# utils
 
 EcoEntry = collections.namedtuple('EcoEntry', ['eco', 'name', 'moves'])
 
 def parse_moves(moves):
     moves = re.split(r'\d+\.', moves)[1:]
     return tuple(ply for move in moves for ply in move.strip().split())
-
-# TODO factor out into lib
-def eco_from_position(openings):
-    position_lookup = {}
-    for eco, name, moves in openings:
-        board = Board()
-        for move in moves:
-            board.push_san(move)
-        position_lookup[board.stateless_fen()] = (eco, name)
-    return position_lookup
-
-# scrape
 
 if __name__ == '__main__':
     response = requests.get('http://www.365chess.com/eco.php')
@@ -45,7 +28,6 @@ if __name__ == '__main__':
         moves = parse_moves(moves.string)
         openings.add(EcoEntry(eco, name, moves))
 
-    with open(os.path.join(config.DATA, 'eco.json'), 'w') as f:
-        for opening in openings:
-            print(json.dumps(opening._asdict()), file=f)
+    for opening in openings:
+        print(json.dumps(opening._asdict()))
 
