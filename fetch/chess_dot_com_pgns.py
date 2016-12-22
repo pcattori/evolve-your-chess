04b2b5661609c175b8a58c1403ef4ad7164af267
Pedro Cattori <pcattori@gmail.com>
2016-12-21 19:21:09 -0500

    argparse instead of sys.argv

--- fetch/chess_dot_com_pgns.py
+++ fetch/chess_dot_com_pgns.py
@@ -4,18 +4,31 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from tqdm import tqdm
+import argparse
 import contextlib
 import datetime
 import getpass
 import glob
 import os
 import selenium
-import sys
 import time
 
-chromedriver_path = sys.argv[1]
-pgns_dir = sys.argv[2]
-# TODO by default last 1000 blitz games, else provide custom url and/or page count
+# argparse
+##########
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', '--driver', help='path to chromedriver executable')
+parser.add_argument('-o', '--output', help='output directory')
+
+#chrome.get('https://www.chess.com/games/archive?gameOwner=my_game&gameType=live')
+parser.add_argument(
+    '-f', '--filter-url', default='https://www.chess.com/games/archive?gameType=live',
+    help='advanced search url from https://www.chess.com/games/archive as a filter')
+parser.add_argument(
+    '-p', '--pages', type=int, default=20,
+    help='number of pages to download. each page has 50 games')
+
+args = parser.parse_args()
 
 print('''Please choose a login option by specifying the number in parentheses:
 (1) chess.com
@@ -30,15 +43,15 @@ username = input('username: ')
 password = getpass.getpass('password for "{}": '.format(username))
 
 # spin up chrome
-if not os.path.exists(chromedriver_path):
+if not os.path.exists(args.driver):
     chromedriver_url = 'https://sites.google.com/a/chromium.org/chromedriver/'
     raise FileNotFoundError('Chromedriver not found. Please install from', chromedriver_url)
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option('prefs', {
-    'download.default_directory' : pgns_dir})
+    'download.default_directory' : args.output})
 with contextlib.closing(webdriver.Chrome(
-        chromedriver_path, chrome_options=options)) as chrome:
+        args.driver, chrome_options=options)) as chrome:
 
     # navigate to login screen
     login_url = 'https://www.chess.com/login'
@@ -69,22 +82,21 @@ with contextlib.closing(webdriver.Chrome(
         sys.exit(3)
 
     # navigate to games archive
-    chrome.get('https://www.chess.com/games/archive?gameOwner=my_game&gameType=live')
+    chrome.get(args.filter_url)
 
-
-    # hit "More" until all your games are loaded
-    for _ in tqdm(range(20)):
+    # hit "Next" for each page
+    for _ in tqdm(range(args.pages)):
         # select all games
         chrome.find_element_by_css_selector('thead input').click()
         chrome.find_element_by_xpath('//a[@title="Download"]').click()
 
         # download selected game PGNs
-        pgns = glob.glob(os.path.join(pgns_dir, '*'))
+        pgns = glob.glob(os.path.join(args.output, '*'))
 
         timeout = datetime.datetime.now() + datetime.timedelta(minutes=5)
         download_success = False
         while datetime.datetime.now() < timeout:
-            if glob.glob(os.path.join(pgns_dir, '*')) != pgns:
+            if glob.glob(os.path.join(args.output, '*')) != pgns:
                 download_success = True
                 break
             time.sleep(1)
